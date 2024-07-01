b9e997578e724aab484ff93aa1be4feb59e02d39
IgorLytkin <igor.lytkin.2020@ya.ru>
2024-07-01 15:14:59 +0700

    Добавил debug-логирование

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.fsm.state import State, StatesGroup
@@ -5,6 +7,8 @@ from aiogram.types import Message, User
 from aiogram_dialog import Dialog, DialogManager, StartMode, Window, setup_dialogs
 from aiogram_dialog.widgets.text import Format
 from environs import Env
+
+logger = logging.getLogger(__name__)
 
 env = Env()
 env.read_env()
@@ -20,6 +24,7 @@ class StartSG(StatesGroup):
 
 
 async def username_getter(dialog_manager: DialogManager, event_from_user: User, **kwargs):
+    logger.debug('username_getter (' + event_from_user.username + ')')
     return {'username': event_from_user.username}
 
 
@@ -34,10 +39,20 @@ start_dialog = Dialog(
 
 @dp.message(CommandStart())
 async def command_start_process(message: Message, dialog_manager: DialogManager):
+    logger.debug('command_start_process' + message.text)
     await dialog_manager.start(state=StartSG.start, mode=StartMode.RESET_STACK)
 
 
 if __name__ == '__main__':
+
+    # Конфигурируем логирование
+    logging.basicConfig(level=logging.DEBUG,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
+
+    # Выводим в консоль информацию о начале запуска бота
+    logger.info('Starting bot')
+
     dp.include_router(start_dialog)
     setup_dialogs(dp)
     dp.run_polling(bot)
